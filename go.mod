ae7e1ced42b560545368299cab6b69d5b688020a
saasan <saasan.s2works@hotmail.com>
2022-08-13 20:15:37 +0000

    初版

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/saasan/remove-empty-folders
+
+go 1.19
+
+require (
+	github.com/saasan/go-dir v0.0.0-20220811155032-2250749128b4
+	github.com/saasan/go-term v0.0.0-20220813181131-2f20e98490b8
+)
+
+require (
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/saasan/go-dir"
+	"github.com/saasan/go-term"
+)
+
+func removeEmptyDir(dirname string) error {
+	dirs, _, err := dir.Read(dirname)
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range dirs {
+		path := filepath.Join(dirname, dir.Name())
+		if err := removeEmptyDir(path); err != nil {
+			return err
+		}
+	}
+
+	isEmpty, err := dir.IsEmpty(dirname)
+	if err != nil {
+		return err
+	}
+
+	if !isEmpty {
+		return nil
+	}
+
+	fmt.Printf("空フォルダを削除: %s\n", dirname)
+	if err := os.Remove(dirname); err != nil {
+		return fmt.Errorf("%s を削除できません。: %w", dirname, err)
+	}
+
+	return nil
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Printf("処理対象: %s\n", arg)
+		if err := removeEmptyDir(arg); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	if runtime.GOOS == "windows" {
+		fmt.Println("Press any key to continue...")
+		term.WaitAnyKey()
+	}
+}
